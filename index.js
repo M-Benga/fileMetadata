56ca4ccb0c658bc5b5487c8f48b3064b9eb3e5a2
Muhammad <hamism77@gmail.com>
2024-11-03 17:57:00 +0300

    First commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+
+// Basic Configuration
+const upload = multer({ dest: 'uploads/' }); 
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -12,7 +16,17 @@ app.get('/', function (req, res) {
 });
 
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+  // Return file metadata as specified
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
